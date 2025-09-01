edf643c6766cd779fa1dcd77a9acb78b842b4cf7
Bruno Rodrigues <rodriguesxd7@hotmail.com>
2025-08-31 21:20:47 -0300

    adicionando calculo de media e soma de numeros

---
+++ src/calculadora.js
@@ -0,0 +1,22 @@
+function somarDoisNumeros(a, b) {
+    return a * b;
+}
+
+function calcularMedia(a, b) {
+    return (a + b) / 2;
+}
+
+function somar () {
+    const valor1 = -10
+    const valor2 = 20
+    if(valor1 < 0 || valor2 < 0) {
+        return "Número inválido"
+    } else {
+        return somarDoisNumeros(valor1, valor2)
+    }
+}
+module.exports = {
+    somarDoisNumeros,
+    calcularMedia,
+    somar
+}
---
+++ tests/calculadorar.teste.js
@@ -0,0 +1,4 @@
+const { somarDoisNumeros } = require("../src/calculadora.js")
+
+const result = somarDoisNumeros(5, 10)
+console.log(result)
